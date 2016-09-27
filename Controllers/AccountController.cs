7bc6b4ba18de0769ae4d4a9796c7efad96795c5b
Salim Bhonhariya <sbhonhariya@accuratetechnologies.com>
2016-09-27 14:51:24 -0400

    First commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace SampleMvcApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// This is just a helper action to enable you to easily see all claims related to a user. It helps when debugging your
+        /// application to see the in claims populated from the Auth0 ID Token
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        public IActionResult Claims()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return new ChallengeResult("Auth0", new AuthenticationProperties() { RedirectUri = "/" });
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Authentication.SignOutAsync("Auth0");
+            HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SampleMvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        // mubina is admin
+        [Authorize(Roles = "admin")]
+        public IActionResult AdministratorIndex()
+        {
+            return View("~/Views/Home/Index1.cshtml");
+
+            // return "All good. You only get this message if you are authenticated.";
+        }
+
+        [Authorize(Roles = "user")]
+        public IActionResult UserIndex()
+        {
+            return View("~/Views/Home/User.cshtml");
+        }
+
+
+    }
+}
